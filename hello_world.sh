44308521c65e044162891086940d6daab8d7ac78
Mayu Katafuchi <mkatafuchi@ufl.edu>
2024-09-13 10:22:43 -0400

    Move print list to a function

--- hello_world.sh
+++ hello_world.sh
@@ -3,3 +3,13 @@
 # This is our first script.
 
 echo 'Hello World!'
+
+function print_list() {
+	# Print the list of files in the directory
+
+	echo 'These are the files in your directory'
+
+	ls
+}
+
+print_list
